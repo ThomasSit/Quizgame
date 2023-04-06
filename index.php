0dfd8f1f79b0b8c7c6f980de74f80180116b995c
ThomasSit <165077@student.horizoncollege.nl>
2023-04-06 08:58:50 +0200

    database update niet af voorbeeld

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
-<?php include './include/header.php'?>
+<?php 
+    include './include/header.php';
+    include './connect/connect.php';
+?>
+
+
 <main>
     <h1>Catergorie</h1>
     <div class="container">
+
+    <?php 
+    // hier de 
+
+
+    foreach($quiz as $quizvragen){
+        $id = $quizvragen['id'];
+    }
+    
+    ?>
      <!-- foreach loop komen uit database button click haal id uit 
     id uit halen  doorgestuurd naar quizpagia uit quizpagina komt daaruit fetch id 
 for loop data quz vragen  -->
